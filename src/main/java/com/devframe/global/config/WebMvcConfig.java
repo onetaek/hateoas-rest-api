e006734792f3ff6501c2ff3cc1124a97c7837ba6
won taek oh <dnjsxorchlrh@naver.com>
2024-05-07 03:57:30 +0900

    [update] 불필요한 코드제거(List형태 validate 동작하는지 테스트했을 때 사용한 코드)

--- src/main/java/com/devframe/global/config/WebMvcConfig.java
+++ src/main/java/com/devframe/global/config/WebMvcConfig.java
@@ -1,7 +1,5 @@
 package com.devframe.global.config;
 
-import com.devframe.global.common.argumentresolver.CustomValidator;
-import com.devframe.global.common.argumentresolver.ListValidationArgumentResolver;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import lombok.RequiredArgsConstructor;
@@ -9,22 +7,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.stereotype.Component;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
-
-    private final CustomValidator customValidator;
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new ListValidationArgumentResolver(customValidator));
-    }
 
     @Bean
     public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder, HttpMethodSerializer httpMethodSerializer) {
